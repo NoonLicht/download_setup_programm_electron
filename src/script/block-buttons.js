a09acd002525b163919fc89f69d8283d020b838e
NoonLicht <121355541+NoonLicht@users.noreply.github.com>
2024-01-25 00:20:38 +0300

    Summary

--- src/script/block-buttons.js
+++ src/script/block-buttons.js
@@ -31,7 +31,7 @@ document.addEventListener("DOMContentLoaded", function () {
 });
 
 // Остальной код кнопок и их обработчиков
-function createIconButton(className, clickHandler, iconPath) {
+function createIconButton(className, clickHandler, iconPath, buttonText) {
   const button = document.createElement("button");
   button.className = "button normal-button";
   button.addEventListener("click", clickHandler);
@@ -55,76 +55,93 @@ function createIconButton(className, clickHandler, iconPath) {
   iconContainer.className = "svg-con";
   iconContainer.appendChild(iconSVG);
 
+  const buttonTextElement = document.createElement("div");
+  buttonTextElement.className = "button-text";
+  buttonTextElement.textContent = buttonText;
+
   button.appendChild(iconContainer);
+  button.appendChild(buttonTextElement);
 
   return button;
 }
+
 
 // Использование функции для создания кнопок
 const chooseFolderButton = createIconButton(
   "button normal-button",
   chooseFolder,
-  "M3 17V7a2 2 0 0 1 2-2h6l2 2h6a2 2 0 0 1 2 2v8a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2Z"
+  "M3 17V7a2 2 0 0 1 2-2h6l2 2h6a2 2 0 0 1 2 2v8a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2Z",
+  "Выбрать папку"
 );
 
 const downloadButton = createIconButton(
   "button normal-button",
   downloadData,
-  "M12 10v6m0 0-3-3m3 3 3-3M3 17V7a2 2 0 0 1 2-2h6l2 2h6a2 2 0 0 1 2 2v8a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2Z"
+  "M12 10v6m0 0-3-3m3 3 3-3M3 17V7a2 2 0 0 1 2-2h6l2 2h6a2 2 0 0 1 2 2v8a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2Z",
+  "Скачать"
 );
 
 const installButton = createIconButton(
   "button normal-button",
   installData,
-  "M17 14v6m-3-3h6M6 10h2a2 2 0 0 0 2-2V6a2 2 0 0 0-2-2H6a2 2 0 0 0-2 2v2a2 2 0 0 0 2 2Zm10 0h2a2 2 0 0 0 2-2V6a2 2 0 0 0-2-2h-2a2 2 0 0 0-2 2v2a2 2 0 0 0 2 2ZM6 20h2a2 2 0 0 0 2-2v-2a2 2 0 0 0-2-2H6a2 2 0 0 0-2 2v2a2 2 0 0 0 2 2Z"
+  "M17 14v6m-3-3h6M6 10h2a2 2 0 0 0 2-2V6a2 2 0 0 0-2-2H6a2 2 0 0 0-2 2v2a2 2 0 0 0 2 2Zm10 0h2a2 2 0 0 0 2-2V6a2 2 0 0 0-2-2h-2a2 2 0 0 0-2 2v2a2 2 0 0 0 2 2ZM6 20h2a2 2 0 0 0 2-2v-2a2 2 0 0 0-2-2H6a2 2 0 0 0-2 2v2a2 2 0 0 0 2 2Z",
+  "Установить"
 );
 
 const installLinux = createIconButton(
   "button normal-button",
   installWSL,
-  "m8 9 3 3-3 3 M13 15h3 M5 20h14a2 2 0 0 0 2-2V6a2 2 0 0 0-2-2H5a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2Z"
+  "m8 9 3 3-3 3 M13 15h3 M5 20h14a2 2 0 0 0 2-2V6a2 2 0 0 0-2-2H5a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2Z",
+  "Установить WSL"
 );
 
 const installHyperV = createIconButton(
   "button normal-button",
   installHyper,
-  "m21 14-9 7-9-7L6 3l3 7h6l3-7 3 11Z"
+  "m21 14-9 7-9-7L6 3l3 7h6l3-7 3 11Z",
+  "Включить Hyper-V"
 );
 
 const GithubLink = createIconButton(
   "button normal-button",
   gitLink,
-  "M15 21v-3.5c0-1 .1-1.4-.5-2 2.8-.3 5.5-1.4 5.5-6a4.6 4.6 0 0 0-1.3-3.2 4.2 4.2 0 0 0-.1-3.2s-1.1-.3-3.5 1.3a12.3 12.3 0 0 0-6.2 0C6.5 2.8 5.4 3.1 5.4 3.1a4.2 4.2 0 0 0-.1 3.2A4.6 4.6 0 0 0 4 9.5c0 4.6 2.7 5.7 5.5 6-.6.6-.6 1.2-.5 2V21h6Z M9 19c-4.3 1.4-4.3-2.5-6-3"
+  "M15 21v-3.5c0-1 .1-1.4-.5-2 2.8-.3 5.5-1.4 5.5-6a4.6 4.6 0 0 0-1.3-3.2 4.2 4.2 0 0 0-.1-3.2s-1.1-.3-3.5 1.3a12.3 12.3 0 0 0-6.2 0C6.5 2.8 5.4 3.1 5.4 3.1a4.2 4.2 0 0 0-.1 3.2A4.6 4.6 0 0 0 4 9.5c0 4.6 2.7 5.7 5.5 6-.6.6-.6 1.2-.5 2V21h6Z M9 19c-4.3 1.4-4.3-2.5-6-3",
+  "Репозиторий"
 );
 
 const GithubLinkDouble = createIconButton(
   "button normal-button",
   gitLink,
-  "M15 21v-3.5c0-1 .1-1.4-.5-2 2.8-.3 5.5-1.4 5.5-6a4.6 4.6 0 0 0-1.3-3.2 4.2 4.2 0 0 0-.1-3.2s-1.1-.3-3.5 1.3a12.3 12.3 0 0 0-6.2 0C6.5 2.8 5.4 3.1 5.4 3.1a4.2 4.2 0 0 0-.1 3.2A4.6 4.6 0 0 0 4 9.5c0 4.6 2.7 5.7 5.5 6-.6.6-.6 1.2-.5 2V21h6Z M9 19c-4.3 1.4-4.3-2.5-6-3"
+  "M15 21v-3.5c0-1 .1-1.4-.5-2 2.8-.3 5.5-1.4 5.5-6a4.6 4.6 0 0 0-1.3-3.2 4.2 4.2 0 0 0-.1-3.2s-1.1-.3-3.5 1.3a12.3 12.3 0 0 0-6.2 0C6.5 2.8 5.4 3.1 5.4 3.1a4.2 4.2 0 0 0-.1 3.2A4.6 4.6 0 0 0 4 9.5c0 4.6 2.7 5.7 5.5 6-.6.6-.6 1.2-.5 2V21h6Z M9 19c-4.3 1.4-4.3-2.5-6-3",
+  "Репозиторий"
 );
 
 const EnableCheck = createIconButton(
   "button normal-button",
   enableCheck,
-  "m9 12 2 2 4-4 M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"
+  "m9 12 2 2 4-4 M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z",
+  "Применить"
 );
 
 const RollBack = createIconButton(
   "button normal-button",
   rollBack,
-  "m15 19-7-7 7-7"
+  "m15 19-7-7 7-7",
+  "Отменить изменения"
 );
 
 const toggleThemeBtn = createIconButton(
-  "button toggle-theme-button",
+  "button normal-button",
   toggleTheme,
-  "M12 3v1m0 16v1m9-9h-1M4 12H3m15.364 6.364-.707-.707M6.343 6.343l-.707-.707m12.728 0-.707.707M6.343 17.657l-.707.707M16 12a4 4 0 1 1-8 0 4 4 0 0 1 8 0Z"
+  "M12 3v1m0 16v1m9-9h-1M4 12H3m15.364 6.364-.707-.707M6.343 6.343l-.707-.707m12.728 0-.707.707M6.343 17.657l-.707.707M16 12a4 4 0 1 1-8 0 4 4 0 0 1 8 0Z",
+  "Сменить тему"
 );
 
 const toggleThemeBtnDouble = createIconButton(
-  "button toggle-theme-button",
+  "button normal-button",
   toggleTheme,
-  "M12 3v1m0 16v1m9-9h-1M4 12H3m15.364 6.364-.707-.707M6.343 6.343l-.707-.707m12.728 0-.707.707M6.343 17.657l-.707.707M16 12a4 4 0 1 1-8 0 4 4 0 0 1 8 0Z"
+  "M12 3v1m0 16v1m9-9h-1M4 12H3m15.364 6.364-.707-.707M6.343 6.343l-.707-.707m12.728 0-.707.707M6.343 17.657l-.707.707M16 12a4 4 0 1 1-8 0 4 4 0 0 1 8 0Z",
+  "Сменить тему"
 );
 
 //----------------------------------------Обработка кнопок----------------------------------------//
